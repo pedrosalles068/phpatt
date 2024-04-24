58b0d97bfdb29b6712acc08f54930a8ec1aa44ae
pedro <pedro.salles@alunos.unis.edu.br>
2024-04-23 21:14:55 -0300

    nova att

--- aulas/aula1sql.sql
+++ aulas/aula1sql.sql
@@ -2,5 +2,5 @@ create table usuarios (
     id serial primary key,
     nome varchar(255) not null,
     email varchar(255) not null,
-    senha varchar(255) not null
+    user varchar(255) not null
 );
